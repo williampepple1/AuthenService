46a0746d7684b1aa743062082fa1e356e8f3046b
William Pepple <williampepple1@gmail.com>
2025-05-16 11:13:00 +0100

    Add User entity and interface

---
+++ AuthenService.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace AuthenService.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastLoginAt { get; set; }
+    }
+}
---
+++ AuthenService.Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using AuthenService.Domain.Entities;
+
+namespace AuthenService.Domain.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByIdAsync(Guid id);
+        Task<User> GetByEmailAsync(string email);
+        Task<User> GetByUsernameAsync(string username);
+        Task<User> CreateAsync(User user);
+        Task UpdateAsync(User user);
+    }
+}
