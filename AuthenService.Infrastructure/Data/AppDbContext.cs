73e48a3e491b28b44f304d06ab855f90a929a59b
William Pepple <williampepple1@gmail.com>
2025-05-16 11:22:44 +0100

    Add AppDbContext and User Repository

---
+++ AuthenService.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using AuthenService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthenService.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.HasKey(e => e.Id);
+                entity.HasIndex(e => e.Email).IsUnique();
+                entity.HasIndex(e => e.Username).IsUnique();
+                entity.Property(e => e.Email).IsRequired();
+                entity.Property(e => e.Username).IsRequired();
+                entity.Property(e => e.PasswordHash).IsRequired();
+            });
+        }
+    }
+}
---
+++ AuthenService.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,50 @@
+﻿using AuthenService.Domain.Entities;
+using AuthenService.Domain.Interfaces;
+using AuthenService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthenService.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetByIdAsync(Guid id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> GetByEmailAsync(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<User> GetByUsernameAsync(string username)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Username == username);
+        }
+
+        public async Task<User> CreateAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task UpdateAsync(User user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
